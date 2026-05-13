833c36815e299067ba5b22344ed89152b8da1155
Minami Ueda <minami.ueda@gmail.com>
2026-05-13 12:20:08 -0400

    Bump version to 0.6.0

--- app/config.py
+++ app/config.py
@@ -34,7 +34,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="EPYDEMIX_")
 
     app_name: str = "epydemix WebAPI"
-    app_version: str = "0.5.1"
+    app_version: str = "0.6.0"
     debug: bool = False
 
     # API settings
